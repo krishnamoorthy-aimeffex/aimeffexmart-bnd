188cb779d3c83bcf8fdc8540dd038d826238cc4d
krishnamoorthy-aimeffex <krishnamoorthy.aimeffex@gmail.com>
2026-03-14 01:39:48 +0530

    Swgger removed

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import swaggerUi from "swagger-ui-express";
-import { swaggerSpec } from "./config/swagger";
 import connectDB from "./config/db";
 import passport from "passport";
 import "./config/passport";
@@ -15,9 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(passport.initialize());
-
-// Swagger UI
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Routes
 import authRoutes from "./routes/auth.routes";
@@ -41,7 +36,6 @@ const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
-    console.log(`📄 Swagger docs at http://localhost:${PORT}/api-docs`);
   });
 });
   
